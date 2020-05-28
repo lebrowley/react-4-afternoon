c86b065e2ca10555ebd7350ea2d92f67a94d152b
lebrowley <brocious123@gmail.com>
2020-05-28 15:04:19 -0600

    assignment complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+import { HashRouter } from 'react-router-dom';
+import routes from './routes';
+import { Link } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <nav className='nav'>
-          <div>WestSide University</div> 
-          <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
-          </div>
-        </nav>
-      </div>
+      <HashRouter>
+        <div>
+          <nav className='nav'>
+
+            <div>WestSide University</div> 
+
+            <div className='link-wrap'>
+              <Link to='/'className='links'>Home</Link>
+              <Link to='/about' className='links'>About</Link>
+            </div>
+
+          </nav>
+          {routes}    {/*so this is how our Home and About components are brought into App.js for rendering? the divs className='links' above are just place holders for the links that take you to other views. Since home is the landing page and Home.js renders the class list, it is now appearing on the landing page because {routes} brought this component in...right? */}
+        </div>
+      </HashRouter>
+     
     )
   }
 }
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class ClassList extends Component {
   constructor() {
     super()
     
+    this.state = {
+      students: []
+    }
   }
 
+  componentDidMount(){
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+    .then(res => {
+      this.setState({students: res.data})
+      // console.log(res.data)
+    })
+    .catch(res => console.log('error getting class info'))
+  }
   render() {
+    const students= this.state.students.map((student, i) => (  //why is it parentheses here? isn't it usually curly braces? 
+      <Link to={`/student/${student.id}`} key={i}>
+        <h3>
+          {student.first_name} {student.last_name}
+        </h3>
+      </Link>
+    ))
+
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {students}      {/*how is it we can just drop this info here? what about styling? if we wanted to target it with styling, we would need to wrap it in a div or some sort of tag, right?? make it a full element? */}
       </div>
     )
   }
-}
+};
